d04b1701dce273408ca6d8bae112400699067a32
Hyungkyu Choi <fabric93@naver.com>
2019-11-30 18:28:53 +0900

    Pycode to get income level per year

--- indicators/income_level.py
+++ indicators/income_level.py
@@ -1,12 +1,9 @@
+# Import Modules
 from pathlib import Path
-from sklearn.linear_model import LinearRegression
-import numpy as np
 import pandas as pd
 
 DATA_DIR = Path('../data/world_development_indicators')
 df_level = pd.read_csv(DATA_DIR/'income_level.csv', delimiter='|')
-# print(df_level[df_level['level'] == 'Low'])
-# print(df_level['1987'][0])
 
 # Read csv from data
 df = pd.read_csv(DATA_DIR/'Indicators.csv')
@@ -17,11 +14,25 @@ df_indicator = df_indicator[['CountryName', 'Year', 'Value']]
 df_pivot = df_indicator.pivot('CountryName', 'Year', 'Value')
 
 # Fill na method using interpolate
-# df_pivot = df_pivot.interpolate(method='linear', limit_direction='both', axis=1)
-# a = pd.melt(df_pivot, id_vars=["CountyName"], value_vars=df_pivot.columns[1:])
-# print(a)
+df_pivot = df_pivot.interpolate(method='linear', limit_direction='both', axis=1)
+list_year = df_indicator[df_indicator['Year'] >= 1987]['Year'].tolist()
+list_value = df_indicator[df_indicator['Year'] >= 1987]['Value'].tolist()
+list_countryName = df_indicator[df_indicator['Year'] >= 1987]['CountryName'].tolist()
 
-"""
-for i in df_pivot.index:
-    temp = df_pivot.loc[i, :]
-"""
+# Comparing with income_level.csv
+list_result = []
+for inx in range(len(list_value)):
+    if list_year[inx] >= 1987:
+        if int(list_value[inx]) <= int((df_level[str(list_year[inx])])[0]):
+            temp = 'Low'
+        elif int(list_value[inx]) <= int((df_level[str(list_year[inx])])[1]):
+            temp = 'Lower-Middle'
+        elif int(list_value[inx]) <= int((df_level[str(list_year[inx])])[2]):
+            temp = 'Upper-Middle'
+        else:
+            temp = 'High-Level'
+        list_result.append(temp)
+
+# Make dataFrame and save to csv
+df_final = pd.DataFrame({'CountryName': list_countryName, 'Year': list_year, 'Income-Level': list_result})
+df_final.to_csv(DATA_DIR/'Country_Income_Level.csv')
