2b270dc4c1c47cc3bbd12fa1a1318d4d6b220f1e
KimGyunYop <gyop0817@naver.com>
2019-12-02 14:55:43 +0900

    cluster all Power set and compute silhouette score

--- indicators/TP_2.py
+++ indicators/TP_2.py
@@ -4,19 +4,25 @@ from pathlib import Path
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.mixture import GaussianMixture
 from sklearn.cluster import KMeans, DBSCAN
+from sklearn.metrics import silhouette_score
+
+def powerset(iterable):
+    from itertools import chain, combinations
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
 DATA_DIR = Path('../data/world_development_indicators')
 key_word_dict = {}
 key_word_dict['Demography'] = ['population','birth','death','fertility','mortality','expectancy']
-key_word_dict['Food'] = ['food','grain','nutrition','calories']
-key_word_dict['Trade'] = ['trade','import','export','good','shipping','shipment']
+#key_word_dict['Food'] = ['food','grain','nutrition','calories']
+#key_word_dict['Trade'] = ['trade','import','export','good','shipping','shipment']
 key_word_dict['Health'] = ['health','desease','hospital','mortality','doctor']
-key_word_dict['Economy'] = ['income','gdp','gni','deficit','budget','market','stock','bond','infrastructure']
+#key_word_dict['Economy'] = ['income','gdp','gni','deficit','budget','market','stock','bond','infrastructure']
 key_word_dict['Energy'] = ['fuel','energy','power','emission','electric','electricity']
 key_word_dict['Education'] = ['education','literacy']
-key_word_dict['Employment'] =['employed','employment','umemployed','unemployment']
-key_word_dict['Rural'] = ['rural','village']
-key_word_dict['Urban'] = ['urban','city']
+#key_word_dict['Employment'] =['employed','employment','umemployed','unemployment']
+#key_word_dict['Rural'] = ['rural','village']
+#key_word_dict['Urban'] = ['urban','city']
 
 origin_dataset = pd.read_csv(DATA_DIR/"Indicators.csv")
 origin_dataset["Country"] = origin_dataset["Year"].map(str) + origin_dataset["CountryName"]
@@ -46,14 +52,49 @@ pivot_data_each_keyword = dict()
 for kw in key_word_dict.keys():
     not_keyword_col = [x if x not in contain_keyword_col[kw] else "" for x in pivot_data.columns]
     not_keyword_col = list(filter(lambda a: a != "", not_keyword_col))
-    print(not_keyword_col)
     pivot_data_each_keyword[kw] = pivot_data.drop(columns = not_keyword_col)
     pivot_data_each_keyword[kw] = pivot_data_each_keyword[kw].dropna()
-
-for kw in key_word_dict.keys():
     print(kw)
-    print(pivot_data_each_keyword[kw].count())
+    print(pivot_data_each_keyword[kw].columns)
+    print(len(pivot_data_each_keyword[kw].columns))
 # More delete columns
+
+keyword_powerset = powerset(key_word_dict.keys())
+
+for keyList in keyword_powerset:
+    tmp_table = pd.DataFrame()
+    if len(keyList) == 0:
+        continue
+    for kw in keyList:
+        if len(tmp_table) == 0:
+            tmp_table = pivot_data_each_keyword[kw]
+        else:
+           tmp_table = pd.merge(tmp_table,pivot_data_each_keyword[kw],right_index=True,left_index=True)
+    print(keyList,"\nrow length = ",len(tmp_table),"\ncolumn count = ",len(tmp_table.columns))
+
+    # Preprocessing
+    # Use more scaler
+    tmp = MinMaxScaler().fit_transform(tmp_table)
+    tmp_table = pd.DataFrame(tmp, columns=tmp_table.columns, index=range(len(tmp_table.index.values)))
+    print(tmp_table.shape)
+
+    # KMeans
+    km = KMeans(n_clusters=6)
+    km_result = km.fit_predict(tmp_table)
+    print(km_result)
+    print(silhouette_score(tmp_table,km_result))
+
+    # EM
+    gm = GaussianMixture(n_components=6)
+    gm_result = gm.fit_predict(tmp_table)
+    print(gm_result)
+    print(silhouette_score(tmp_table,gm_result))
+
+    # DBSCAN
+    ds = DBSCAN(eps=0.1, min_samples=4)
+    ds_result = ds.fit_predict(tmp_table)
+    print(ds_result)
+    print(silhouette_score(tmp_table,ds_result))
 '''
 # Preprocessing
 # Use more scaler
