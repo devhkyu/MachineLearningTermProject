4ee972f55b4f26134151d0d78ac32757d62f3446
KimGyunYop <gyop0817@naver.com>
2019-12-03 11:46:28 +0900

    cal purity

--- indicators/TP_2.py
+++ indicators/TP_2.py
@@ -5,11 +5,37 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.mixture import GaussianMixture
 from sklearn.cluster import KMeans, DBSCAN
 from sklearn.metrics import silhouette_score
+from sklearn.model_selection import GridSearchCV
+from collections import Counter
+import operator
+import json
 
 def powerset(iterable):
     from itertools import chain, combinations
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+
+def silhouette_scoring(estimator, x):
+    cluster = estimator.fit_predict(x)
+    score = silhouette_score(x,cluster)
+    return score
+
+def cal_purity(model, x, gold):
+    model.fit(x)
+    count_el = dict(sorted(Counter(model.labels_).items(), key=operator.itemgetter(0)))  # count each cluster's volume
+    clustering_result = dict()  # collect each cluster's number of each elements
+    gold["cluster"] = model.labels_
+    popul = 0
+
+    all_len = 0
+    # cal each cluster
+    for i in count_el.keys():
+        cluster_counter = Counter(gold.loc[gold["cluster"] == i, "Income-Level"])
+        clustering_result[i] = dict(cluster_counter)
+        popul += max(cluster_counter.values())
+        all_len += sum(cluster_counter.values())
+
+    return float(popul)/all_len
 
 DATA_DIR = Path('../data/world_development_indicators')
 key_word_dict = {}
@@ -24,17 +50,21 @@ key_word_dict['Education'] = ['education','literacy']
 #key_word_dict['Rural'] = ['rural','village']
 #key_word_dict['Urban'] = ['urban','city']
 
+km_hpTune = {"n_clusters":[4,6,8,10,12],"max_iter":[50,100,200]}
+gm_hpTune = {"n_components":[4,6,8,10,12],"covariance_type":['full','tied','diag','spherical'], "max_iter":[50,100,200]}
+dc_hpTune ={"eps":[0.001,0.01,0.05,0.1,0.2,0.5],"min_samples":[30,50,100,200]}
+
 origin_dataset = pd.read_csv(DATA_DIR/"Indicators.csv")
 origin_dataset["Country"] = origin_dataset["Year"].map(str) + origin_dataset["CountryName"]
-
+incomeLevel_dataset = pd.read_csv(DATA_DIR/"Country_Income_Level.csv",index_col=0)
+incomeLevel_dataset["Country"] = incomeLevel_dataset["Year"].map(str) + incomeLevel_dataset["CountryName"]
+incomeLevel_dataset = incomeLevel_dataset.drop(columns=["Year","CountryName"])
 # Reshape data
 pivot_data = origin_dataset.pivot("Country","IndicatorName","Value")
 pivot_data.columns = [x.lower() for x in pivot_data.columns]
-print(pivot_data.columns)
 
 # Find not nan data over than 700
 over_7000_indicator = pivot_data.count()[pivot_data.count()>7000].keys()
-print(over_7000_indicator)
 
 contain_keyword_col = dict()
 for i in key_word_dict.keys():
@@ -45,7 +75,6 @@ for kw in key_word_dict.keys():
     for i in over_7000_indicator:
         if any(ext in i for ext in key_word_dict[kw]):
             contain_keyword_col[kw].append(i)
-print(contain_keyword_col)
 
 # Drop feature of above columns
 pivot_data_each_keyword = dict()
@@ -61,6 +90,8 @@ for kw in key_word_dict.keys():
 
 keyword_powerset = powerset(key_word_dict.keys())
 
+hyper_tune = dict()
+purity_result = dict()
 for keyList in keyword_powerset:
     tmp_table = pd.DataFrame()
     if len(keyList) == 0:
@@ -69,51 +100,53 @@ for keyList in keyword_powerset:
         if len(tmp_table) == 0:
             tmp_table = pivot_data_each_keyword[kw]
         else:
-           tmp_table = pd.merge(tmp_table,pivot_data_each_keyword[kw],right_index=True,left_index=True)
+            tmp_table = pd.merge(tmp_table,pivot_data_each_keyword[kw],right_index=True,left_index=True)
     print(keyList,"\nrow length = ",len(tmp_table),"\ncolumn count = ",len(tmp_table.columns))
 
     # Preprocessing
     # Use more scaler
     tmp = MinMaxScaler().fit_transform(tmp_table)
-    tmp_table = pd.DataFrame(tmp, columns=tmp_table.columns, index=range(len(tmp_table.index.values)))
+    tmp_table = pd.DataFrame(tmp, columns=tmp_table.columns, index=tmp_table.index.values)
     print(tmp_table.shape)
 
-    # KMeans
-    km = KMeans(n_clusters=6)
-    km_result = km.fit_predict(tmp_table)
-    print(km_result)
-    print(silhouette_score(tmp_table,km_result))
+    hyper_tune[keyList] = dict()
+    purity_result[keyList] = dict()
 
-    # EM
-    gm = GaussianMixture(n_components=6)
-    gm_result = gm.fit_predict(tmp_table)
-    print(gm_result)
-    print(silhouette_score(tmp_table,gm_result))
+    tmp_incomeLevel = incomeLevel_dataset[incomeLevel_dataset["Country"].isin(tmp_table.index.values)].reset_index()
+    print(tmp_table.index.values)
+    tmp_table = tmp_table.reindex(tmp_incomeLevel["Country"])
+
+    # KMeans
+    km = KMeans()
+    km_search = GridSearchCV(estimator=km, param_grid=km_hpTune, scoring=silhouette_scoring, n_jobs=3, cv=5, verbose=10)
+    km_result = km_search.fit(tmp_table)
+    print(km_result.best_params_)
+    print(km_result.best_score_)
+    hyper_tune[keyList]["km"] = km_result
+
+    km_best = KMeans(**(km_result.best_params_))
+    purity = cal_purity(km_best,tmp_table,tmp_incomeLevel)
+    print(purity)
+    purity_result[keyList]["km"] = purity
+
+
+    # gausian navie basis
+    gm = GaussianMixture()
+    gm_search = GridSearchCV(estimator=gm, param_grid=gm_hpTune, scoring=silhouette_scoring, n_jobs=3, cv=5, verbose=10)
+    gm_result = gm_search.fit(tmp_table)
+    print(gm_result.best_params_)
+    print(gm_result.best_score_)
+    hyper_tune[keyList]["gm"] = gm_result
 
     # DBSCAN
-    ds = DBSCAN(eps=0.1, min_samples=4)
-    ds_result = ds.fit_predict(tmp_table)
-    print(ds_result)
-    print(silhouette_score(tmp_table,ds_result))
-'''
-# Preprocessing
-# Use more scaler
-tmp = MinMaxScaler().fit_transform(pivot_data)
-pivot_data = pd.DataFrame(tmp, columns=pivot_data.columns, index=range(len(pivot_data.index.values)))
-print(pivot_data.shape)
+    ds = DBSCAN()
+    ds_search = GridSearchCV(estimator=ds, param_grid=dc_hpTune, scoring=silhouette_scoring, n_jobs=3, cv=5, verbose=10)
+    ds_result = ds_search.fit(tmp_table)
+    print(ds_result.best_params_)
+    print(ds_result.best_score_)
+    hyper_tune[keyList]["ds"] = ds_result
 
-# KMeans
-km = KMeans()
-km_result = km.fit_predict(pivot_data)
-print(km_result)
+print(hyper_tune)
+print(purity_result)
 
-# EM
-gm = GaussianMixture()
-gm_result = gm.fit_predict(pivot_data)
-print(gm_result)
-
-# DBSCAN
-ds = DBSCAN()
-ds_result = ds.fit_predict(pivot_data)
-print(ds_result)
-'''
+open
